cb791b1d2544389d83f455a8d6e95d058419a98c
S-PetersNZ <137010917+S-PetersNZ@users.noreply.github.com>
2024-06-15 18:12:42 +1200

    Solution for leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
-};
+const leapYears = function(inputYear) {
+    let bool = false;
+      if (inputYear % 4 == 0) {
+        bool = true;
+      }
+      if (inputYear % 4 == 0 && inputYear % 100 == 0) {
+        bool = false;
+      }
+      if (inputYear % 4 == 0 && inputYear % 400 == 0) {
+        bool = true;
+      }
+      if (bool == true) {
+        return bool;
+      } else {
+        return bool;
+      }
+  };
 
 // Do not edit below this line
 module.exports = leapYears;
