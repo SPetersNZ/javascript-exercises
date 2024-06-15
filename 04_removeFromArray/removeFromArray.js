71e2dc356117977da02cd59cd2c326eabccfc607
S-PetersNZ <137010917+S-PetersNZ@users.noreply.github.com>
2024-06-15 14:36:15 +1200

    Solution for removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(inputArr, requiredItemToRemove,
+...optionalItemsToRemove) {
+        let i = 0;
+        while (i < inputArr.length) {
+            if (inputArr[i] === requiredItemToRemove || 
+            optionalItemsToRemove.includes(inputArr[i])) {
+                inputArr.splice(i, 1);
+            } else {
+                i++;
+            }
+        }
+        return inputArr;
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;
