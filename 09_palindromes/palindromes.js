ce9dc298c5c00be672807ae0a38ae99d0cb9b13d
SPetersNZ <137010917+SPetersNZ@users.noreply.github.com>
2024-07-01 14:06:57 +1200

    ***INCOMPLETE*** - Solution for palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr1 = str.split("");
+    let arr2 = arr1.slice().reverse();          // .slice creates a new string
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
 };
 
 // Do not edit below this line
