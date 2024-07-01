baf525a3efdee252e0f80f05593b7cb595fa2483
SPetersNZ <137010917+SPetersNZ@users.noreply.github.com>
2024-07-02 10:43:12 +1200

    Solution for fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    if (input < 0) {
+        return "OOPS";
+    }
+    if (input == 0) {
+        return 0;
+    } 
+    let fibArr = [];
+    fibArr[0] = 1;
+    fibArr[1] = 1;
+    for (let i = 2; i < input; i++) {
+        fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
+    }
+    return fibArr[fibArr.length - 1];
 };
 
 // Do not edit below this line
