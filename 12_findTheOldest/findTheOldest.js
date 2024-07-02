1177a589784a75226519013346c6c6c3e5857332
SPetersNZ <137010917+SPetersNZ@users.noreply.github.com>
2024-07-03 09:39:41 +1200

    Solution for findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest
+    const currentYear = (new Date()).getFullYear();
+    for (let person of people) {
+        if (!person.hasOwnProperty("yearOfDeath")) {
+            oldest = people.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) >
+            (currentYear - b.yearOfBirth) ? 1 : -1);
+        } else {
+            oldest = people.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) >
+            (b.yearOfDeath - b.yearOfBirth) ? 1 : -1);
+        }
+        return oldest[oldest.length - 1];
+    }
 };
 
 // Do not edit below this line
